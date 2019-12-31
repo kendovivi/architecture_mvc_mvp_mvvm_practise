957623f2ba9db69c2f1123b107b27d85261cb9cf
Zheng Wang <kendovivi@hotmail.com>
2019-12-31 13:21:01 +0900

    add error case in mvc (#2)
    * add error case for mvc

--- app/src/main/java/com/example/architectures_practise/mvc/MVCActivity.java
+++ app/src/main/java/com/example/architectures_practise/mvc/MVCActivity.java
@@ -8,7 +8,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.architectures_practise.R;
@@ -22,6 +24,8 @@ public class MVCActivity extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
     private ListView listView;
     private CountriesController controller;
+    private Button retryBtn;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,8 @@ public class MVCActivity extends AppCompatActivity {
         controller = new CountriesController(this);
 
         listView = findViewById(R.id.list);
+        retryBtn = findViewById(R.id.btn_retry);
+        progressBar = findViewById(R.id.progress);
         adapter = new ArrayAdapter<>(this, R.layout.row_item_layout, R.id.tv_name, listValues);
         listView.setAdapter(adapter);
 
@@ -43,10 +49,40 @@ public class MVCActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * list viewのデータ設定 controllerでデータ取得完了した時点で呼ばれる
+     *
+     * @param values
+     */
     public void setListValues(List<String> values) {
         listValues.clear();
         listValues.addAll(values);
+        retryBtn.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
+        listView.setVisibility(View.VISIBLE);
         adapter.notifyDataSetChanged();
+
+    }
+
+    /**
+     * retry btnクリック controllerに処理を依頼する
+     */
+    public void onRetry(View view) {
+        controller.doRefresh();
+
+        retryBtn.setVisibility(View.GONE);
+        listView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 失敗時処理
+     */
+    public void onError() {
+        Toast.makeText(this, getString(R.string.error_get_countries), Toast.LENGTH_SHORT).show();
+        retryBtn.setVisibility(View.VISIBLE);
+        listView.setVisibility(View.GONE);
+        progressBar.setVisibility(View.GONE);
     }
 
     public static Intent getIntent(Context context) {
