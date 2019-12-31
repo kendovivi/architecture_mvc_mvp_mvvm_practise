eefdb25320cf2fdd601a1ad60c49c05591ae4f8d
kendovivi <kendovivi@gmail.com>
2019-12-31 12:46:23 +0900

    Add MVC view and success case

---
+++ app/src/main/java/com/example/architectures_practise/mvc/CountriesController.java
@@ -0,0 +1,44 @@
+package com.example.architectures_practise.mvc;
+
+import com.example.architectures_practise.model.CountriesService;
+import com.example.architectures_practise.model.Country;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.observers.DisposableSingleObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class CountriesController {
+
+    private MVCActivity view;
+    private CountriesService service;
+
+    public CountriesController(MVCActivity view) {
+        this.view = view;
+        service = new CountriesService();
+        fetchCountries();
+    }
+
+    private void fetchCountries() {
+        service.getCountries()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableSingleObserver<List<Country>>() {
+                    @Override
+                    public void onSuccess(List<Country> value) {
+                        List<String> countriesNames = new ArrayList<>();
+                        for (Country country: value) {
+                            countriesNames.add(country.countryName);
+                        }
+                        view.setListValues(countriesNames);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+                });
+    }
+}
--- app/src/main/java/com/example/architectures_practise/mvc/MVCActivity.java
+++ app/src/main/java/com/example/architectures_practise/mvc/MVCActivity.java
@@ -5,16 +5,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.architectures_practise.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MVCActivity extends AppCompatActivity {
+
+    private List<String> listValues = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private ListView listView;
+    private CountriesController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mvc);
         setTitle("MVC");
+
+        controller = new CountriesController(this);
+
+        listView = findViewById(R.id.list);
+        adapter = new ArrayAdapter<>(this, R.layout.row_item_layout, R.id.tv_name, listValues);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MVCActivity.this, "you click" + listValues.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void setListValues(List<String> values) {
+        listValues.clear();
+        listValues.addAll(values);
+        adapter.notifyDataSetChanged();
     }
 
     public static Intent getIntent(Context context) {
